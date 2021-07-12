0f394292abbb37c1cfc6fabd8531443938881fff
HelloJeong <jeongyk92@gmail.com>
2021-07-13 01:43:19 +0900

    강의 결과물

---
+++ prototype-shop/README.md
@@ -0,0 +1,5 @@
+# ![React](https://ko.reactjs.org/favicon.ico) [**React**](https://reactjs.org/ "React 공식 홈페이지")
+
+_Fastcampus React 강의 내용을 정리해둔 자료입니다._
+
+## React로 만드는 쇼핑몰 프로젝트
---
+++ prototype-shop/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Prototypes from "./components/Prototypes";
+import Orders from "./components/Orders";
+import Footer from "./components/Footer";
+import AppStateProvider from "./providers/AppStateProvider";
+
+export default function App() {
+  return (
+    <AppStateProvider>
+      <Header />
+      <div className="container">
+        <Prototypes />
+        <Orders />
+        <Footer />
+      </div>
+    </AppStateProvider>
+  );
+}
